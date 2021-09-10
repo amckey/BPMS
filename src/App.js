df1cf40305ee12aa52e9a1bc2b5f01a934eae0d7
sofija.nikitina <sofija.nikitina@softlex.pro>
2021-09-10 12:28:39 +0300

    mend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import {HashRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router } from "react-router-dom";
+import {ConnectedRouter} from "connected-react-router";
+import { history } from "./store/configureStore";
 import Routes from "./Routes";
 import './fixtures/i18next';
 
 function App() {
   return (
       <Router>
-        <Routes/>
+        <ConnectedRouter history={history}>
+           <Routes/>
+        </ConnectedRouter>
       </Router>
   );
 }
